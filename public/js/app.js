f2201125818342182ea954ca93c575a0e54808c3
Molly Haughey <mollyhaughey@Mollys-MBP.cable.rcn.com>
2020-09-30 15:47:45 -0400

    form added

--- public/js/app.js
+++ public/js/app.js
@@ -11,12 +11,25 @@ class App extends React.Component {
     <div class="main-section">
     <div className="container-2">
       <h5>Rent a Movie</h5>
+      <form></form>
+      <label for="title">Title:</label>
+        <input type="text" name="title" value="" /><br/>
+        <label for="year">Year:</label>
+        <input type="text" name="year" value="" />
+        <br/>
+        <label for="rated">Rated:</label>
+        <input type="text" name="rated" value="" />
+        <br/>
+        <label for="genre">Genre:</label>
+        <input type="text" name="genre" value="" />
+        <br/>
+        <input type="submit" value="Submit"></input>
     </div>
     <div className="container-2">
-      <h5>Movies Recently <br/>Added to <br/>Request List</h5>
+      <h5>List of <br/> Open Requests</h5>
     </div>
     <div className="container-2">
-      <h5>Search Results</h5>
+      <h5>Accepted Requests <br/>Coming Soon</h5>
     </div>
     </div>
     </div>

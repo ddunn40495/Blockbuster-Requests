f8720d0a2639b17ff04ed0e59bb89d269f1b988a
Molly Haughey <mollyhaughey@Mollys-MBP.cable.rcn.com>
2020-09-30 15:25:12 -0400

    all divs added

--- public/js/app.js
+++ public/js/app.js
@@ -3,9 +3,22 @@ class App extends React.Component {
 
   render = () => {
     return (
+    <div>
     <div className="container-1">
       <div className="header-img"> 
       </div>
+    </div>
+    <div class="main-section">
+    <div className="container-2">
+      <h5>Rent a Movie</h5>
+    </div>
+    <div className="container-2">
+      <h5>Movies Recently <br/>Added to <br/>Request List</h5>
+    </div>
+    <div className="container-2">
+      <h5>Search Results</h5>
+    </div>
+    </div>
     </div>
     )
     };
